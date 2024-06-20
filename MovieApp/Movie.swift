c9a8292a774afddb31909e95812c4fa4737cb7c5
Ahmed-emad11 <ahmed.fares01025@gmail.com>
2024-06-20 08:08:41 +0300

    Initial commit

---
+++ MovieApp/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Ahmed Emad on 19/6/2024.
+//
+
+import Foundation
+class Movie {
+    var title: String?
+    var image: String?
+    var rating: Double?
+    var releaseYear: Int?
+
+}
---
+++ MovieApp/MovieCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCell.swift
+//  MovieApp
+//
+//  Created by Ahmed Emad on 19/6/2024.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    @IBOutlet var movieImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+}
